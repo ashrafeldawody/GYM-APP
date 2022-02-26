089fe10110a9c82cf1d4ebee8ec56833fb74cbf1
Ayman Mohamed Hosny Kheir-Eldeen <Aymanmido067@gmail.com>
2022-02-27 00:15:23 +0200

    Creating Purshase Controller and Requests

---
+++ app/Http/Controllers/PurchaseController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Purchase;
+use App\Http\Requests\StorePurchaseRequest;
+use App\Http\Requests\UpdatePurchaseRequest;
+
+class PurchaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // This method has to return a datatables view that has these columns
+        // user_name | user_email | package_name \ amount_user_paid | gym | city
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // This  will show the view of  buy_package_for_user form
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePurchaseRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePurchaseRequest $request)
+    {
+        // This method will store a new package record in the database
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Purchase  $purchase
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Purchase  $purchase
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePurchaseRequest  $request
+     * @param  \App\Models\Purchase  $purchase
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePurchaseRequest $request, Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Purchase  $purchase
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Purchase $purchase)
+    {
+        //
+    }
+}
