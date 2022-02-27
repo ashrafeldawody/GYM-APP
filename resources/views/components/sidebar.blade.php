290811887faa4ebe25aeafe1b72b6336d87d3de8
Ashraf Eldawody <ashraf6450@gmail.com>
2022-02-27 22:25:08 +0200

    Sidebar update

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div>
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <a href="index3.html" class="brand-link">
-            <img src="" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <a href="#" class="brand-link">
+            <img src="{{url('dist/img/AdminLTELogo.png')}}" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">{{ config('app.name', 'Laravel App') }}</span>
         </a>
 
