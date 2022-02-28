303ad5a36ea772e2580344e8c383e80d73a373fb
Ashraf Eldawody <ashraf6450@gmail.com>
2022-03-01 00:30:06 +0200

    fix dashboard route and path conflict

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .copy('node_modules/admin-lte/dist/img', 'public/dashboard/dist/img')
+    .copy('node_modules/admin-lte/dist/img', 'public/dist/img')
     .sourceMaps();
