60f83c6f860b149d17a0799de7fc9b7570fc2196
Ashraf Eldawody <ashraf6450@gmail.com>
2022-02-28 22:35:33 +0200

    fixing sidebar layout

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar sidebar-dark-primary elevation-4 position-fixed">
         <a href="#" class="brand-link">
             <img src="{{url('dist/img/AdminLTELogo.png')}}" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">{{ config('app.name', 'Laravel App') }}</span>
