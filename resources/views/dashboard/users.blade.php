abf1e8aef93ab4c7ab012e562f95f76c7ea929d6
amraabdallah <amr.abdalaziz.abdallah@gmail.com>
2022-02-28 20:25:55 +0200

    Add UserDataTable

--- resources/views/dashboard/users.blade.php
+++ resources/views/dashboard/users.blade.php
@@ -2,6 +2,50 @@
 
 @section('content')
 <div class="container">
-    <h1>Inherting from layouts</h1>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('Users Table') }}</div>
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+                    <div class="">
+                        {{$dataTable->table()}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+
+<script type="text/javascript">
+    $(function () {
+    var table = $('#users-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "{{ route('dashboard.users.index') }}",
+        columns: [
+            {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+            {data: 'name', name: 'name'},
+            {data: 'email', name: 'email'},
+            {data: 'nationalId', name: 'nationalId'},
+            {data: 'gym_name', name: 'gym_name'},
+
+            {
+                data: 'action',
+                name: 'action',
+                orderable: true,
+                searchable: true
+            },
+        ]
+    });
+  });
+</script>
