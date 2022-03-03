24cd93b08d3cfbdb270659779a8eacb8744ad172
Ahmed Hafez <ahmedhafez13@yahoo.com>
2022-03-04 01:59:07 +0200

    Add update to cityController with custom request

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -7,7 +7,7 @@ use App\Models\City;
 use App\Http\Requests\StoreCityRequest;
 use App\Http\Requests\UpdateCityRequest;
 use App\Http\Resources\CityResource;
-use Illuminate\Support\Facades\DB;
+use App\Models\Manager;
 use Yajra\DataTables\Facades\DataTables;
 
 class CityController extends Controller
@@ -32,24 +32,25 @@ class CityController extends Controller
 
     public function getFormData()
     {
+        $managers = Manager::get(['id', 'name'])->toArray();
+
         return [
             'formLable' => 'City',
             'fields' => [
                 [
                     'label' => 'City Name',
                     'name' => 'name',
-                    'type' => 'text'
+                    'type' => 'text',
+                    'value' => 'name'
                 ],
                 [
                     'label' => 'Manager',
                     'name' => 'manager_id',
                     'type' => 'select',
-                    'options' => [
-                        ['value' => '1', 'text' => 'AAA'],
-                        ['value' => '2', 'text' => 'BBB'],
-                        ['value' => '3', 'text' => 'CCC'],
-                        ['value' => '4', 'text' => 'DDD'],
-                    ]
+                    'value' => 'id',
+                    'text' => 'name',
+                    'compare' => 'manager_name',
+                    'options' => $managers
                 ]
             ]
         ];
@@ -107,7 +108,13 @@ class CityController extends Controller
      */
     public function update(UpdateCityRequest $request, City $city)
     {
-        //
+        $city->update($request->validated());
+        $newCityData = Datatables::of(CityResource::collection([$city]))->make(true);
+        return [
+            'result' => true,
+            'userMessage' => "<b>{$city->name}</b> has beed successfully updated",
+            'updatedData' => $newCityData
+        ];
     }
 
     /**
