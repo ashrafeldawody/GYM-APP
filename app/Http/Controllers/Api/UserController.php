68efc5c3aae8a525dee1b179f67866ae5ef267b5
amraabdallah <amr.abdalaziz.abdallah@gmail.com>
2022-03-03 14:46:35 +0200

    Hashed password on registeration has been added

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\StoreUserRequest;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 
 class UserController extends Controller
@@ -26,8 +28,23 @@ class UserController extends Controller
 
     public function store(StoreUserRequest $request){
         //caution > [password_confirmation] must be in the request
+
+        request()->merge([
+            'password' => Hash::make(request()->password),
+        ]);
+
         $requestData = request()->all();
         $newUser = User::create($requestData);
+
+        // $newUser = User::create([
+        //     'name' => request()->name,
+        //     'gender' => request()->gender,
+        //     'email' => request()->email,
+        //     'password' => Hash::make(request()->password),
+        //     'birth_date' => request()->birth_date,
+        //     'profile_img' => request()->profile_img,
+        // ]);
+
         return $newUser;
     }
 }
