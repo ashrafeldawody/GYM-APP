295ca5982b662b1a917d3cc9b2c4577e457c3521
Ayman Mohamed Hosny Kheir-Eldeen <Aymanmido067@gmail.com>
2022-03-03 19:01:24 +0200

    Adding permissions to resources routes

--- routes/web.php
+++ routes/web.php
@@ -29,17 +29,17 @@ Route::prefix('dashboard')->middleware('auth')->name('dashboard.')->group(functi
 
     Route::get('/', function () {return view('home');})->name('home');
 
-    Route::resource('cities', CityController::class);
+    Route::resource('cities', CityController::class)->middleware('permission:cities');
 
-    Route::resource('purchases', PurchaseController::class)->only(['index', 'create', 'store']);
+    Route::resource('purchases', PurchaseController::class)->only(['index', 'create', 'store'])->middleware('permission:purchases');
 
-    Route::resource('attendance', AttendanceController::class)->only(['index']);
+    Route::resource('attendance', AttendanceController::class)->only(['index'])->middleware('permission:attendance');
 
-    Route::resource('packages', PackagesController::class);
+    Route::resource('packages', PackagesController::class)->middleware('permission:packages');
 
-    Route::resource('sessions', SessionsController::class);
+    Route::resource('sessions', SessionsController::class)->middleware('permission:sessions');
 
-    Route::resource('users', UserController::class);
+    Route::resource('users', UserController::class)->middleware('permission:users');
 
     Route::prefix('account')->name('account.')->group(function () {
         Route::get('/', [ManagerController::class,'index'])->name('index');
