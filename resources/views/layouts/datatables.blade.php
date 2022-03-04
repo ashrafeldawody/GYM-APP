24b3342f2a7fe06032a5c5dbbd01a49fc9cc88f6
Ahmed Hafez <ahmedhafez13@yahoo.com>
2022-03-04 02:00:26 +0200

    Add add and edit functionality with messages and validations

--- resources/views/layouts/datatables.blade.php
+++ resources/views/layouts/datatables.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12 mb-5">
             <div class="card">
-                <div class="card-header">@yield('table_header')</div>
+                <div class="card-header bg-light sticky-top">@yield('table_header')</div>
                 <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
@@ -24,13 +24,25 @@
                                 <button id="deleteButton" class="btn btn-danger mr-2"><i
                                         class="fa fa-trash mr-2"></i>Delete</button>
                             </div>
-                            <button id="deleteButton" class="btn btn-success"><i
-                                    class="fa fa-plus mr-2"></i>Add</button>
+                            <button id="addButton" class="btn btn-success"><i class="fa fa-plus mr-2"></i>Add</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
+
+<div class="toast shadow-lg m-3 fade hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="3500"
+    style="position: fixed; right: 0; top: 0; margin-top: 1rem !important; z-index: 99999;">
+    <div class="toast-header">
+        <strong id="toastTitle" class="mr-auto" style="min-width: 200px"></strong>
+        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="toast-body">
+        <p id="toastMessage"></p>
     </div>
 </div>
 
@@ -45,10 +57,32 @@
             </div>
             <div class="modal-body">
                 <form id="addEditForm"></form>
+                <div id="alertsDiv"></div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Update</button>
+                <button id="formConfirmBtn" class="btn btn-primary">Update</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmTitle">Confirm</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p id="confirmMessage"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button id="confirmDeleteBtn" type="button" class="btn btn-danger">Delete</button>
             </div>
         </div>
     </div>
@@ -64,15 +98,24 @@
     $(function () {
         const ajaxUrl = @yield('table_route', 'null');
         const formDataEndpoint = @yield('form_data_endpoint', 'null');
+        const addEndpoint = @yield('add_endpoint', 'null');
+        const updateEndpoint = @yield('update_endpoint', 'null');
 
         if (!ajaxUrl) return;
 
         const controlsPanel = $('#controlsPanel');
+        const addButton = $('#addButton');
         const editButton = $('#editButton');
         const deleteButton = $('#deleteButton');
         const userMessage = $('#userMessage');
         const formElem = $('#addEditForm');
         const formLable = $('#formModalLabel');
+        const formConfirmBtn = $('#formConfirmBtn');
+        const alertsDiv = $('#alertsDiv');
+        const confirmModal = $('#confirmModal');
+        const confirmMessage = $('#confirmMessage');
+
+        if (!addEndpoint) addButton.hide();
 
         let datatable = $('#datatable').DataTable({
             bAutoWidth: false,
@@ -104,7 +147,7 @@
             order: [[ 1, 'asc' ]]
         });
 
-        datatable.on('select deselect', function (e, dt, type, indexes) {
+        datatable.on('select deselect draw.dt', function (e, dt, type, indexes) {
             const selectedCount = datatable.rows('.selected').data().length;
             toggleControlPanel(selectedCount > 0);
         });
@@ -117,48 +160,199 @@
             }
         });
 
-        editButton.click(function () {
+        addButton.click(function() {
+            formConfirmBtn.off().click(submitAdd);
+            formConfirmBtn.attr('class', 'btn btn-success');
+            formConfirmBtn.html('Add');
+            showFormModal(false);
+        });
+
+        editButton.click(function() {
+            formConfirmBtn.off().click(submitEdit);
+            formConfirmBtn.attr('class', 'btn btn-primary');
+            formConfirmBtn.html('Update');
+            showFormModal(true);
+        });
+
+        deleteButton.click(showDeleteConfirm);
+
+        $('#confirmDeleteBtn').click(confirmDelete);
+
+        // ----- * ----- * ----- * ----- * ----- * ----- * -----
+        // ----- * ----- *      Add / Edit       * ----- * -----
+        // ----- * ----- * ----- * ----- * ----- * ----- * -----
+
+        function showFormModal(isToEdit) {
             $.ajax({
                 url: formDataEndpoint,
                 method: 'GET'
             })
             .done(function(formData) {
-                // Set the form label
-                formLable.html('Edit ' + formData.formLable);
+                // get data of selected row if isToEdit
+                let selectedRow = null;
+                if (isToEdit) {
+                    const selectedRows = datatable.rows('.selected').data();
+                    selectedRow = selectedRows.length > 0 ? selectedRows[0] : null;
+                }
 
-                // Create the html form fields
-                let formFields = '';
-                formData.fields.forEach(field => {
-                    if (field.type == 'text') {
-                        formFields +=
-                            `<div class="form-group">
-                                <label for="${field.name}Input" class="col-form-label">${field.label}</label>
-                                <input type="text" name="${field.name}" class="form-control" id="${field.name}Input">
-                            </div>`;
-                    } else if (field.type == 'select') {
-                        formFields +=
-                            `<div class="form-group">
-                                <label for="${field.name}Input" class="col-form-label">${field.label}</label>
-                                <select name="${field.name}" class="form-control">
-                                    <option disabled>Select ${field.label}</option>
-                                    ${
-                                        field.options.map(option =>
-                                            `<option value="${option.value}">${option.text}</option>`
-                                        ).join("")
-                                    }
-                                </select>
-                            </div>`;
-                    }
-                });
-                formElem.html(formFields);
+                // Generate form fields
+                createForm(formData, selectedRow, isToEdit);
 
                 // Show the modal
                 $('#formModal').modal().show();
             });
-        });
+        }
 
-        deleteButton.click(function () {
+        function createForm(formData, selectedRow = null, isToEdit) {
+            // Set the form label
+            formLable.html((isToEdit ? 'Edit ' : 'Add ') + formData.formLable);
+
+            // Create the html form fields
+            let formFields = '';
+
+            formData.fields.forEach(field => {
+                if (field.type == 'text' || field.type == 'email') {
+                    formFields += createTextField(field, selectedRow);
+                } else if (field.type == 'select') {
+                    formFields += createSelectField(field, selectedRow);
+                } else if (field.type == 'time') {
+                    formFields += createTimeField(field, selectedRow);
+                } else if (field.type == 'date') {
+                    formFields += createDateField(field, selectedRow);
+                }
+            });
+
+            // set formFields html in the form
+            formElem.html(formFields);
+        }
+
+        function createTextField(field, selectedRow) {
+            const textValue = selectedRow ? selectedRow[field.value] : '';
+            return `<div class="form-group">
+                    <label for="${field.name}_input" class="col-form-label">${field.label}</label>
+                    <input type="${field.type}" name="${field.name}" value="${textValue}" class="form-control" id="${field.name}_input">
+                </div>`;
+        }
+
+        function createSelectField(field, selectedRow) {
+            const selectedOption = selectedRow ? selectedRow[field.compare] : '';
+            return `<div class="form-group">
+                    <label class="col-form-label">${field.label}</label>
+                    <select name="${field.name}" class="form-control">
+                        <option disabled>Select ${field.label}</option>
+                        ${
+                            field.options.map(option =>
+                                `<option value="${option[field.value]}" ${selectedOption == option[field.text] ? 'selected' : ''}>
+                                    ${option[field.text]}
+                                </option>`
+                            ).join("")
+                        }
+                    </select>
+                </div>`;
+        }
+
+        function createTimeField(field, selectedRow) {
+            const timeValue = selectedRow ? selectedRow[field.value] : '';
+            console.log('timeValue', timeValue);
+            return `<div class="form-group">
+                    <label for="${field.name}_input" class="col-form-label">${field.label}</label>
+                    <input type="time" name="${field.name}" value="${timeValue}" class="form-control" id="${field.name}_input">
+                </div>`;
+        }
+
+        function createDateField(field, selectedRow) {
+
+        }
+
+        // ----- * ----- * ----- * -----
+
+        function submitEdit() {
+            let data = formElem.serialize();
+
             const itemId = datatable.rows('.selected').data()[0].id;
+
+            $.ajax({
+                url: updateEndpoint + `/${itemId}`,
+                method: 'PATCH',
+                data: data
+            })
+            .done(function(response) {
+                handleEditSuccess(response);
+            })
+            .fail(function(response) {
+                handleEditFail(response);
+            });
+        }
+
+        function handleEditSuccess(response) {
+            datatable.rows('.selected').data(response.updatedData).draw(false);
+            datatable.rows('.selected').deselect();
+            $('#formModal .close').click();
+            showSuccessToast('Edit success', response.userMessage);
+        }
+
+        function handleEditFail(response) {
+            let message = response.responseJSON.message;
+            let errors = response.responseJSON.errors;
+            let errorsAlerts = '<div class="alert alert-danger" role="alert">';
+                errorsAlerts += `<p><strong>${message}</strong></p>`;
+            for (const error in errors) {
+                errorsAlerts += `<div>${errors[error]}</div>`;
+            }
+            errorsAlerts += '</div>';
+            alertsDiv.html(errorsAlerts);
+        }
+
+        // ----- * ----- * ----- * -----
+
+        function submitAdd() {
+            let data = formElem.serialize();
+
+            $.ajax({
+                url: addEndpoint,
+                method: 'POST',
+                data: data
+            })
+            .done(function(response) {
+                handleAddSuccess(response);
+            })
+            .fail(function(response) {
+                handleAddFail(response);
+            });
+        }
+
+        function handleAddSuccess(response) {
+
+        }
+
+        function handleAddFail(response) {
+
+        }
+
+        // ----- * ----- * ----- * ----- * ----- * ----- * -----
+        // ----- * ----- *        Delete         * ----- * -----
+        // ----- * ----- * ----- * ----- * ----- * ----- * -----
+
+        function showDeleteConfirm() {
+            let selectedData = datatable.rows('.selected').data();
+            if (!selectedData.length) return;
+
+            let rowData = '<p class="text-danger">Are you sure, data will be deleted</p>';
+            let columns = datatable.settings().init().columns;
+            datatable.columns().every(function(index) {
+                let columnName = columns[index].name;
+                if (columnName && columnName != 'DT_RowIndex') {
+                    let headerText = datatable.column(index).header().textContent;
+                    let data = selectedData[0][columnName];
+                    rowData += `<div><b>${headerText}:</b> ${data}</div>`;
+                }
+            });
+            confirmMessage.html(rowData);
+            confirmModal.modal().show();
+        }
+
+        function confirmDelete() {
+            itemId = datatable.rows('.selected').data()[0].id;
             if (itemId) {
                 toggleControlPanel(false);
                 $.ajax({
@@ -168,16 +362,20 @@
                 .done(function(response) {
                     if (response.result == true) {
                         datatable.row('.selected').remove().draw(true);
-                        showSuccessMessage(response.userMessage);
+                        $('#confirmModal .close').click();
+                        showSuccessToast('Delete success', response.userMessage);
                     } else {
                         datatable.rows('.selected').deselect();
-                        showErrorMessage(response.userMessage);
+                        $('#confirmModal .close').click();
+                        showErrorToast('Delete failed', response.userMessage);
                     }
                 });
             }
-        });
+        }
 
-        // ----- * ----- * ----- * -----
+        // ----- * ----- * ----- * ----- * ----- * ----- * -----
+        // ----- * ----- *       Controls        * ----- * -----
+        // ----- * ----- * ----- * ----- * ----- * ----- * -----
 
         function toggleControlPanel(show) {
             show ? showControlPanel() : hideControlPanle();
@@ -186,7 +384,7 @@
         function showControlPanel() {
             controlsPanel.show();
             const selectedCount = datatable.rows('.selected').data().length;
-            formDataEndpoint ? editButton.show() : editButton.hide();
+            updateEndpoint ? editButton.show() : editButton.hide();
             showInfoMessage(`${selectedCount} items has been selected`);
         }
 
@@ -202,14 +400,18 @@
             userMessage.html(message);
         }
 
-        function showSuccessMessage(message) {
-            userMessage.attr('class', 'text-success');
-            userMessage.html(message);
+        function showSuccessToast(title, message) {
+            $('.toast #toastTitle').html(title);
+            $('.toast #toastMessage').attr('class', 'text-success');
+            $('.toast #toastMessage').html(message);
+            $('.toast').toast('show');
         }
 
-        function showErrorMessage(message) {
-            userMessage.attr('class', 'text-danger');
-            userMessage.html(message);
+        function showErrorToast(title, message) {
+            $('.toast #toastTitle').html(title);
+            $('.toast #toastMessage').attr('class', 'text-danger');
+            $('.toast #toastMessage').html(message);
+            $('.toast').toast('show');
         }
     });
 </script>
