416da9712be1147adbb1e1f793bcf8c69978d515
Ahmed Hafez <ahmedhafez13@yahoo.com>
2022-03-05 01:00:47 +0200

    Fix side bar scroll

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <aside class="main-sidebar sidebar-dark-primary elevation-4 position-fixed">
+    <aside class="main-sidebar sidebar-dark-primary elevation-4 position-fixed h-100">
         <a href="#" class="brand-link">
             <img src="{{url('dist/img/AdminLTELogo.png')}}" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">{{ config('app.name', 'Laravel App') }}</span>
