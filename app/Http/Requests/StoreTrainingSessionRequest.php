c185e3f93e4fed5ccec5d4ceb95a7ddf8dc2250c
Ayman Mohamed Hosny Kheir-Eldeen <Aymanmido067@gmail.com>
2022-03-10 12:31:49 +0200

    Finishing Training Session validation

--- app/Http/Requests/StoreTrainingSessionRequest.php
+++ app/Http/Requests/StoreTrainingSessionRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
@@ -25,7 +26,30 @@ class StoreTrainingSessionRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|unique:training_sessions',
+            'day' => 'required|date|after:yesterday',
+            'starts_at' => 'required',
+            'finishes_at' => 'required|after:starts_at',
+            'coach_id' => 'required',
+        ];
+    }
+
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'The session name is required',
+            'day.required' => 'Day Field is required',
+            'day.after' => 'The Entered day is expired',
+            'starts_at.required' => 'Start Time is required',
+            'finishes_at.required' => 'Finish Time is required',
+            'finishes_at.after' => 'Finish Time must be after start time !',
+            'coach_id.required' => 'You have to select at least one coach',
         ];
     }
 }
