b4bd8535b2ec2e5078e2f5c69365909f739d830c
Ashraf Eldawody <ashraf6450@gmail.com>
2022-03-11 22:31:53 +0200

    Cleaning Avatar Link + Changing User Menu

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -10,7 +10,7 @@
         <div class="sidebar">
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="{{asset(Auth::user()->avatar ? url(URL::to('/') . '/'. Auth::user()->avatar) : '/dist/img/avatar.png')}}" class="img-circle elevation-2" alt="User Image">
+                    <img src="{{ Auth::user()->avatar }}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">{{ Auth::user()->name }}</a>
